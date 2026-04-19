2e88ae20fbb1a022cf8358f09d2f8527d00f1b14
kuesterolaf <kuester.olaf@googlemail.com>
2026-04-19 15:51:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,6 @@ from google.oauth2.service_account import Credentials
 
 # --- KONFIGURATION ---
 st.set_page_config(page_title="Kicken beginnt im Kopf", page_icon="⚽", layout="wide")
-st.title("⚽ Kicken beginnt im Kopf")
 st.subheader("Die Sommer-Leseliga des FLVW") # Hier fügen wir den Untertitel ein
 st.markdown("---")
 LIMIT_MINUTEN = 20
