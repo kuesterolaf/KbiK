f09b29a2c05f94b294721d4bca24f60f1e3a7e9f
kuesterolaf <kuester.olaf@googlemail.com>
2026-04-19 18:48:23 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ st.markdown("""
 
     /* Header Zentrierung */
     .header-container { text-align: center; width: 100%; }
-    .tight-title { margin-top: -15px !important; line-height: 1.1; text-align: center; color: #31333F !important; }
+    .tight-title { margin-top: -80px !important; line-height: 1.1; text-align: center; color: #31333F !important; }
     .tight-subtitle { margin-top: -10px !important; color: #666 !important; text-align: center; }
 
     /* Metriken Hauptbereich */
